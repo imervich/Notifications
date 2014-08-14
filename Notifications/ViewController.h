e27a4649671204a02164a87b5867b88ae42d68c0
Iván Mervich <imervich@gmail.com>
2014-08-14 10:24:47 -0500

    Initial Commit

---
+++ Notifications/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Notifications
+//
+//  Created by Iván Mervich on 8/14/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Notifications/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Notifications
+//
+//  Created by Iván Mervich on 8/14/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
